0e40a66ecf9d55a354558b5b991488ca26d4ac50
Sarah Belsky <sarahbelsky1@gmail.com>
2023-12-05 21:48:42 -0500

    day 4

--- day4/solution.js
+++ day4/solution.js
@@ -2,16 +2,40 @@ const fs = require("fs");
 const filePath = "input.txt";
 
 fs.readFile(filePath, "utf8", (err, data) => {
-  const input = data.split("\r");
+  const input = data.split("\n");
 
-  console.log("Part 1: \n" + part1(input));
-  console.log("Part 2: \n" + part2(input));
+  console.log("Part 1: " + part1(input));
+  console.log("Part 2: " + part2(input));
 });
 
 const part1 = (input) => {
-  return input;
+  return input
+    .map((line) => {
+      const [, winNums, cardNums] = line
+        .split(/\s*[:||]\s*/)
+        .map((part) => part.split(/\s+/));
+      const matchCount = cardNums.filter((str) => winNums.includes(str)).length;
+      return matchCount ? Math.pow(2, matchCount - 1) : 0;
+    })
+    .reduce((prev, curr) => prev + curr);
 };
 
 const part2 = (input) => {
-  return;
+  return input
+    .map((line) => {
+      const [, winNums, cardNums] = line
+        .split(/\s*[:||]\s*/)
+        .map((part) => part.split(/\s+/));
+      return {
+        qty: 1,
+        numMatches: cardNums.filter((num) => winNums.includes(num)).length,
+      };
+    })
+    .map((card, cardIdx, allCards) => {
+      for (let idx = 0; idx < card.numMatches; idx++) {
+        allCards[cardIdx + idx + 1].qty += card.qty;
+      }
+      return card.qty;
+    })
+    .reduce((prev, curr) => prev + curr);
 };
