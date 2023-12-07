7a5ca296aa555f593631aac89d11ac837437abc0
Sarah Belsky <sarahbelsky1@gmail.com>
2023-12-06 21:58:58 -0500

    day 5, part 2

--- day5/solution.js
+++ day5/solution.js
@@ -3,16 +3,16 @@ const filePath = "input.txt";
 // const filePath = "testInput.txt";
 
 fs.readFile(filePath, "utf8", (err, data) => {
-  console.log("Part 1: \n" + part1(data));
+  console.log("Part 1: " + part1(data));
   console.log("Part 2: " + part2(data));
 });
 
 const part1 = (input) => {
-  let [seeds, ...charts] = input.split(/[\n|\r]+.*:\n+/);
+  let [seeds, ...charts] = input.split(/[\n|\r]+.*:[\r]*\n+/);
   let srcs = seeds.split(": ")[1].split(" ");
   charts
     .map((chart) =>
-      chart.split(/\n/).map((interval) => {
+      chart.split(/[\r]*\n/).map((interval) => {
         const [dest, src, len] = interval.split(" ").map((val) => +val);
         return { min: src, max: src + len - 1, offset: dest - src };
       })
@@ -30,5 +30,97 @@ const part1 = (input) => {
 };
 
 const part2 = (input) => {
-  return;
+  let [seeds, ...charts] = input.split(/[\n|\r]+.*:[\r]*\n+/);
+  let srcRanges = seeds
+    .split(": ")[1]
+    .split(" ")
+    .map((start, i, arr) => ({
+      min: +start,
+      max: +start + +arr[i + 1] - 1,
+    }))
+    .filter((_, i) => i % 2 == 0);
+
+  charts
+    .map((chart) =>
+      chart
+        .split(/[\r]*\n/)
+        .map((interval) => {
+          const [dest, src, len] = interval.split(" ").map((val) => +val);
+          return { min: src, max: src + len - 1, offset: dest - src };
+        })
+        .sort((a, b) => a.min - b.min)
+    )
+    .forEach((chart) => {
+      const destRanges = srcRanges.flatMap((srcRange) =>
+        getDestRanges(srcRange, chart)
+      );
+      srcRanges = [...destRanges];
+    });
+  return srcRanges
+    .map((locRange) => locRange.min)
+    .reduce((a, b) => Math.min(a, b));
 };
+
+const getDestRanges = (srcRange, mappings) => {
+  let newRanges = [];
+  let rangeListIdx = 0;
+  let minPointer = srcRange.min;
+  let max, offset;
+  if (srcRange.min > mappings[mappings.length - 1].max) {
+    return srcRange;
+  }
+  rangeListIdx = mappings.findIndex((mapping) => srcRange.min < mapping.max);
+  while (minPointer <= srcRange.max) {
+    const mapRange = mappings[rangeListIdx];
+    // console.log(mapRange);
+    if (!mapRange) {
+      max = srcRange.max;
+      offset = 0;
+    } else if (isInRange(minPointer, mapRange)) {
+      max = Math.min(srcRange.max, mapRange.max);
+      offset = mapRange.offset;
+      rangeListIdx++;
+    } else {
+      max = Math.min(srcRange.max, mapRange.min);
+      offset = 0;
+    }
+    newRanges.push({
+      min: minPointer + offset,
+      max: max + offset,
+    });
+    minPointer = max + 1;
+  }
+  // console.log("out of while");
+  return newRanges;
+};
+
+const isInRange = (pointer, range) => {
+  return pointer >= range.min && pointer <= range.max;
+};
+
+// const getDestRanges = (srcRange, mappings) => {
+//   let newRanges = [];
+//   let minPointer = srcRange.min;
+//   while (minPointer <= srcRange.max) {
+//     const matchingRange = mappings.find(
+//       (range) => minPointer >= range.min && minPointer <= range.max
+//     );
+//     if (!matchingRange) {
+//       const nextRange = mappings.find((range) => minPointer < range.min);
+//       if (!nextRange) {
+//         newRanges.push({ min: minPointer, max: srcRange.max });
+//         minPointer = srcRange.max + 1;
+//       } else {
+//         const rangeMax = nextRange.min - 1;
+//         newRanges.push({ min: minPointer, max: rangeMax });
+//         minPointer = rangeMax + 1;
+//       }
+//     } else {
+//       const offset = matchingRange.offset;
+//       const rangeMax = Math.min(matchingRange.max, srcRange.max);
+//       newRanges.push({ min: minPointer + offset, max: rangeMax + offset });
+//       minPointer = rangeMax + 1;
+//     }
+//   }
+//   return newRanges;
+// };
